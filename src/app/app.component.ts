aed2bbf0793d4df196b42dc2b6fb46b64938763a
Jorge <jorgediaz83@gmail.com>
2020-03-03 22:58:04 -0600

    myfirstapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+
+  name: string = 'John Carter';
+  age: number = 28;
+
+  users: string [] = ['jorge', 'pancho', 'pedro'];
+  posts = [];
+
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+  deleteUser(user){
+    for(let i = 0; i < this.users.length; i++){
+      if(user == this.users[i]){
+        this.users.splice(i,1);
+      }
+    }
+  }
+
+  addUser(newUser){
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+
 }
